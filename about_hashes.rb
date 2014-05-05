4ddca7709cecfd3cd0b5316aa64bb1f9d5a4cf27
Tom Brennan <tommybrennan@gmail.com>
2014-05-05 11:36:54 -0400

    koan 37 completed

--- about_hashes.rb
+++ about_hashes.rb
@@ -47,7 +47,7 @@ class AboutHashes < Neo::Koan
     hash1 = { :one => "uno", :two => "dos" }
     hash2 = { :two => "dos", :one => "uno" }
 
-    assert_equal __, hash1 == hash2
+    assert_equal hash2 == hash1, hash1 == hash2
   end
 
   def test_hash_keys
